ed57f0973aece83b9f39a332d465d760c00b1523
krylovolegv <krylovolegv@yandex.ru>
2024-06-11 12:14:38 +0200

    add new anketa

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,28 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect
 from app import app
 
-posts = []
+# Данные анкеты хранятся в списке
+entries = []
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        title = request.form.get('title')
-        content = request.form.get('content')
-        if title and content:
-            posts.append({'title': title, 'content': content})
-        return redirect(url_for('index'))
-    return render_template('blog.html', posts=posts)
+        # Получаем данные из формы
+        name = request.form['name']
+        city = request.form['city']
+        hobby = request.form['hobby']
+        age = request.form['age']
+
+        # Добавляем данные анкеты в список
+        entries.append({
+            'name': name,
+            'city': city,
+            'hobby': hobby,
+            'age': age
+        })
+
+        return redirect('/')
+
+    return render_template('blog.html', entries=entries)
+
