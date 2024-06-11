7381f100482ba67583980c112949c5979938997f
krylovolegv <krylovolegv@yandex.ru>
2024-06-11 10:54:42 +0200

    add _init_ code

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+#создаёт экземпляр класса Flask (переменную app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you will never guess'
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,1 @@
+from render_template, request, redirect, url_for
