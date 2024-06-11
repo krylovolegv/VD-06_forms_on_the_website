693bca28011d151514f7c95eda72db323d7d06b6
krylovolegv <krylovolegv@yandex.ru>
2024-06-11 12:01:18 +0200

    add end code

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-#создаёт экземпляр класса Flask (переменную app)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you will never guess'
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
-from render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from app import app
 
-@app.route('/')
+posts = []
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if title and content:
+            posts.append({'title': title, 'content': content})
+        return redirect(url_for('index'))
+    return render_template('blog.html', posts=posts)
